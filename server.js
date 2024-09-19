c295164e95de73557a3ec6656b83314b26e87918
gentleinvestors9 <gentleinvestors90@gmail.com>
2024-09-19 22:39:12 +0100

    Update server.js

--- server.js
+++ server.js
@@ -12,6 +12,7 @@ const PORT = process.env.PORT || 5000; // port to connect to WEB
 // emails credentials
 const userEmail = "gentleinvestors90@gmail.com";
 const pass = "ysqwnfquhattkzbt";
+// real 15 oct
 
 // Middleware
 app.use(express.json());
